a816a320fe9e476827a44533cc7f8ac54328307b
Mieko <miekotominaga@gmail.com>
2023-11-17 15:20:44 -0500

    initial commit

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,19 +1,19 @@
 import "./Card.scss";
 import chevron from "../../assets/icons/Chevron.png";
-import star from "../../assets/icons/Icon.png";
 
 let Card = (props) => {
   const star = "⭐";
 
-  // let rating = star.repeat(props.numStars);
-
   return (
     <li className="card">
-      <img className="card__image" src={props.image} />
+      <img
+        className="card__image"
+        src={props.image}
+        alt={`${props.publisher} logo`}
+      />
       <div className="card__info">
         <article className="header">
           <h1 className="header__title">{props.name}</h1>
-          {/* <img className="header__rating" src={star.repeat(props.numStars)} /> */}
           <div className="header__rating">{star.repeat(props.numStars)}</div>
           <article className="author">
             <p>By&nbsp;</p>
@@ -35,7 +35,7 @@ let Card = (props) => {
       </div>
       <div className="card__more">
         <p className="card__more__text">More</p>
-        <img className="card__more__chevron" src={chevron} />
+        <img className="card__more__chevron" src={chevron} alt="arrow" />
       </div>
     </li>
   );
